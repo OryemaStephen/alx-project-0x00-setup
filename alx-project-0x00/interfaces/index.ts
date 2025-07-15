bb7c5a615f76092a4d08a13e73deb1e733a6320a
OryemaStephen <oryemasteve@gmail.com>
2025-07-15 22:41:16 +0300

    Updated styles prop

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -4,7 +4,7 @@ export interface PillProps {
 
 export interface ButtonProps {
   title: string;
-  styles?: React.CSSProperties;
+  styles: React.CSSProperties;
   onClick?: () => void;
   className?: string;
   size?: 'small' | 'medium' | 'large';
